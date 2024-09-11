feac3bcea827dfdd62f13ee86478c88c0de23e27
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-11 14:19:42 +0300

    refactored to use redux state

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,23 +1,26 @@
-const Notification = ({ message, type }) => {
-    if (message === null) {
-        return null;
-    }
-    const successStyle = {
-        color: 'hsl(98, 97%, 36%)',
-        background: 'rgba(211, 211, 211, 0.418)',
-    };
-    const errorStyle = {
-        color: 'hsla(357, 90%, 43%, 0.801)',
-        background: 'rgba(211, 211, 211, 0.336)',
-    };
+import { useSelector } from 'react-redux';
 
-    const style = type === 'error' ? errorStyle : successStyle;
+const Notification = () => {
+    const notification = useSelector((state)=>state.notification)
 
-    return (
-        <div className="notification-message" style={style}>
-            {message}
-        </div>
-    );
+  if (!notification.message) return null
+  
+  const successStyle = {
+    color: 'hsl(98, 97%, 36%)',
+    background: 'rgba(211, 211, 211, 0.418)',
+  };
+  const errorStyle = {
+    color: 'hsla(357, 90%, 43%, 0.801)',
+    background: 'rgba(211, 211, 211, 0.336)',
+  };
+
+  const style = notification.type === 'error' ? errorStyle : successStyle;
+
+  return (
+    <div className={`${notification.type} notification`} style={style}>
+      {notification.message}
+    </div>
+  );
 };
 
 export default Notification;
