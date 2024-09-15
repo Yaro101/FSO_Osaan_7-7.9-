46fce6fbc3dc0ca5331fc47dba4764a1371d4030
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-15 16:32:21 +0300

    refactored code + removed store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
