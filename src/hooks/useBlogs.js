610a8b5135e26f6dfc71ef5a39f4458b1407af87
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-15 16:39:26 +0300

    refactored code made clearer and fixed typos

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -3,32 +3,38 @@ import blogService from '../services/blogs'
 
 // Retrieving the blogs using Query
 export const useBlogs = () => {
-    return useQuery('blogs', blogService.getAll)
+    return useQuery({
+        queryKey: ['blogs'],
+        queryFn: blogService.getAll,
+    })
 }
 
 export const useCreateBlog = () => {
     const queryClient = useQueryClient()
-    return useMutation(blogService.create, {
+    return useMutation({
+        mutationFn: blogService.create,
         onSuccess: () => {
-            queryClient.invalidateQueries('blogs') // for refreshing the bloglist after addition
+            queryClient.invalidateQueries({ queryKey: ['blogs'] }) // for refreshing the bloglist after addition
         }
     })
 }
 
 export const useUpdateBlog = () => {
     const queryClient = useQueryClient()
-    return useMutation(blogService.update, {
-        onSuccess: ()=>{
-            queryClient.invalidateQueries('blogs')
+    return useMutation({
+        mutationFn: blogService.update,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['blogs'] })
         }
     })
 }
 
 export const useDeleteBlog = () => {
     const queryClient = useQueryClient()
-    return useMutation(blogService.remove, {
-        onSuccess: ()=>{
-            queryClient.invalidateQueries('blogs')
+    return useMutation({
+        mutationFn: blogService.remove,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['blogs'] })
         }
     })
 }
