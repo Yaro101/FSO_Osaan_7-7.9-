a2660704b1f345f39239517bafb59baf1a499afe
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-17 15:42:04 +0300

    login returns username, name and fake token

--- src/services/login.js
+++ src/services/login.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
-const baseUrl = '/login';
 
-const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials);
-    return response.data;
+const baseUrl = 'http://localhost:3001/users';
+
+const login = async (credentials) => {
+    // Fetch all users
+    const response = await axios.get(baseUrl);
+    const users = response.data;
+    const user = users.find((u) => u.username === credentials.username);
+    return { username: user.username, name: user.name, token: 'fake-jwt-token' };
 };
 
 export default { login };
