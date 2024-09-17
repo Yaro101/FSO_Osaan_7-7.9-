24b47ccf3ecc29afd20681de49cf95452af97caa
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-17 15:42:59 +0300

    fixed typo causing  a bug

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,48 +1,52 @@
 import PropTypes from 'prop-types';
 
 const LoginForm = ({
-    handleSubmit,
-    handleUsernameChange,
-    handlePasswordChange,
-    username,
-    password,
+  handleSubmit: handleFormSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password,
 }) => {
-    return (
-        <div>
-            <h2>Log in to application</h2>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleFormSubmit({ username, password });
+  };
 
-            <form onSubmit={handleSubmit}>
-                <div>
+  return (
+    <div>
+      <h2>Log in to application</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
           username
-                    <input
-                        data-testid="username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        // onChange={({ target }) => setUsername(target.value)}
-                    />
-                </div>
-                <div>
-          password
-                    <input
-                        data-testid="password"
-                        type="password"
-                        value={password}
-                        // onChange={({ target }) => setPassword(target.value)}
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="submit">login</button>
-            </form>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            id="username"
+          />
         </div>
-    );
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            id="password"
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
 };
 
 LoginForm.propTypes = {
-    handleSubmit: PropTypes.func.isRequired,
-    handleUsernameChange: PropTypes.func.isRequired,
-    handlePasswordChange: PropTypes.func.isRequired,
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
 
 export default LoginForm;
