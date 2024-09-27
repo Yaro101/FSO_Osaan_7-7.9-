252d5cea1f20510480307aef823d380707ec1621
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-27 12:25:58 +0300

    Added get all users to userBlogs services

--- src/services/UserBlogs.js
+++ src/services/UserBlogs.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:3001/users';
 
+// Fetch all users
+const getAllUsers = async () =>{
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+// Update user blog services
 const updateUserBlogsService = async ({ id, blog }) => {
     const response = await axios.patch(`${baseUrl}/${id}`, {
         blog, // sending the new blog
@@ -9,4 +16,4 @@ const updateUserBlogsService = async ({ id, blog }) => {
     return response.data; // 
 };
 
-export default { updateUserBlogsService };
+export default { getAllUsers, updateUserBlogsService };
